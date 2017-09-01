08cc25060c4afc8567473e6947dc16764b8fa3be
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-09-01 03:57:43 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,22 +1,17 @@
-# encoding: utf-8
 require 'rubygems'
 require 'bundler/setup'
 
 require 'sinatra'
-require 'sinatra/base'
-set :run, false
-set :server, %w[thin mongrel webrick]
-set :bind, '0.0.0.0'
-set :port, 8585
+
+
+set :run, true
+set :bind,'0.0.0.0'
+set :port, 80
+
+
+
   set :root, File.dirname(__FILE__)
 
-class App < Sinatra::Base
-
-
   get '/' do
-"hello"
+"hello joe"
   end
-
-end
-
-
