58dddad2ddb92600d13d84f42b11be53c143fbab
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-09-13 00:01:46 -0400

    Working with mike and , am trying changes he wants
    to see if works on mie

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "Start Server"
+(cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd /var/www && nohup sudo bundle exec rackup -p80 -o 0.0.0.0 &)
+echo "working?"
